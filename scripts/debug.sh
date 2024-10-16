5a53dbb50ff9c411b7fc29f5ce203a35e13557b1
navazjm <michaelnavs@gmail.com>
2024-10-16 12:37:35 -0500

    updated debug build and script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p build/debug
-cd build/debug
+cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build/debug
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
+make -C build/debug
 
-cmake --build .
+# Run the executable if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Debug - Build succeeded!"
+else
+    echo "Debig - Build failed!"
+    exit 1
+fi
 
-cd ../..
